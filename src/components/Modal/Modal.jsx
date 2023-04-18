c720f936173503003cfe2b03b82990cc35c330bc
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 18:27:45 -0500

    Add test to Modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,10 @@ import styles from './Modal.module.scss';
 
 const Modal = ({ children, isOpen, onClose }) => {
   return (
-    <div className={isOpen ? `${styles.modal} ${styles.open}` : styles.modal}>
+    <div
+      className={isOpen ? `${styles.modal} ${styles.open}` : styles.modal}
+      role="dialog"
+    >
       <div className={styles.content}>
         <button className={styles.close} onClick={onClose}>
           X
