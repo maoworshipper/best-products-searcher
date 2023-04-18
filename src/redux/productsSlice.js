5412035c71ce5676b552a8402bb953a83c712036
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 13:01:10 -0500

    Add singleItem action to productsSlice

--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -1,22 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  query: '',
-  results: [],
-};
+const initialState = {};
 
 export const productsSlice = createSlice({
   name: 'products',
   initialState,
   reducers: {
-    addQuery: (state, action) => {
-      state.query = action.payload;
-    },
     addResults: (state, action) => {
-      state.results = action.payload;
+      state.results = action.payload.results;
+      state.query = action.payload.query;
+      state.paging = action.payload.paging;
+      state.sort = action.payload.sort;
+      state.available_filters = action.payload.available_filters;
+      state.available_sorts = action.payload.available_sorts;
+      state.filters = action.payload.filters;
     },
+    addSingleItem: (state, action) => {
+      state.item = action.payload;
+    }
   },
 });
 
-export const { addQuery, addResults } = productsSlice.actions;
+export const { addResults, addSingleItem } = productsSlice.actions;
 export default productsSlice.reducer;
