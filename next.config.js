b1e93995244844e19d0a05801df7524d420170de
MauricioMartinez <maoworshipper@gmail.com>
2023-04-17 23:06:37 -0500

    Configure next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ['api.mercadolibre.com', 'http2.mlstatic.com'],
+    deviceSizes: [768, 992, 1200],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
