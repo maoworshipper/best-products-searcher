d69922328f7b70b0b463127ee240a6fcb8a2500d
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 18:16:24 -0500

    Add filters option to search api url

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
   try {
-    const { query, offset } = req.query;
-    let url = `${process.env.BACKEND_URL_SEARCH}/search?q=${query}`;
+    const { query, offset, filters, filter } = req.query;
+    let url = `${process.env.BACKEND_URL_SEARCH}search?q=${query}`;
     offset && (url += `&offset=${offset}`);
+    filters && filter && (url += `&${filters}=${filter}`);
     const response = await fetch(url);
     const data = await response.json();
     return res.status(200).json(data);
