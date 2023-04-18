f62109f53472395386642504f8da0061a291346b
MauricioMartinez <maoworshipper@gmail.com>
2023-04-17 23:06:07 -0500

    Configure eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  tabs: false,
+  bracketSpacing: true,
+  arrowParens: "always",
+};
