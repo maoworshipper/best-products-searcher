e4144fc07c85c09ce29f5ecaa005e8b55e9e5602
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 18:28:44 -0500

    Add tests to Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,7 @@ import styles from './Sidebar.module.scss';
 const Sidebar = ({ title = '', children }) => {
   return (
     <>
-      <div className={styles.sidebar}>
+      <div className={styles.sidebar} role="navigation">
         <h3 className={styles.title}>{title}</h3>
         {children}
       </div>
