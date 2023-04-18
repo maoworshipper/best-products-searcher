42ac83f44c62408d19f3d9e0e4e113b04743c544
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 18:11:24 -0500

    Create ProductCard component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,17 @@
 import Image from 'next/image';
 import styles from './Card.module.scss';
 
-const Card = ({ id, title, condition, price, thumbnail, handleClick }) => {
+export const Card = ({ id, title, condition, price, thumbnail, handleClick }) => {
   return (
     <div className={styles.card}>
       <div className={styles.cardImage}>
         <Image src={thumbnail} alt={title} width={100} height={100}  />
       </div>
       <div className={styles.cardContent}>
-        <h3 className={styles.title} onClick={() => handleClick(id)}>{title?.length > 50 ? `${title?.substring(0, 50)}...` : title}</h3>
+        <h3 className={styles.title} onClick={() => handleClick(id)}>{title?.length > 80 ? `${title?.substring(0, 80)}...` : title}</h3>
         <p className={styles.condition}>{condition}</p>
         <p className={styles.price}>$ {price}</p>
       </div>
     </div>
   );
 };
-
-export default Card;
