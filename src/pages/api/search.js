849abdad0d26178972d70acb8f541f862b1136fa
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 12:57:54 -0500

    Add offset option to query in products api

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   try {
-    const { query } = req.query;
-    const url = `${process.env.BACKEND_URL_SEARCH}/search?q=${query}}`;
+    const { query, offset } = req.query;
+    let url = `${process.env.BACKEND_URL_SEARCH}/search?q=${query}`;
+    offset && (url += `&offset=${offset}`);
     const response = await fetch(url);
     const data = await response.json();
     return res.status(200).json(data);
