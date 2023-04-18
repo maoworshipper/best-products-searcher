5400744d64749694f387ad985bba05147248c068
MauricioMartinez <maoworshipper@gmail.com>
2023-04-18 18:14:40 -0500

    Create single item next api handler

---
+++ src/pages/api/item.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+    try {
+      const { query } = req.query;
+      const url = `${process.env.BACKEND_URL_ITEM}${query}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+  
