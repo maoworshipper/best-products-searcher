c451d5684ba7ab1562690df598beaf317675b790
MauricioMartinez <maoworshipper@gmail.com>
2023-04-19 12:40:33 -0500

    Update search next api to accept all parameters

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,9 +1,7 @@
 export default async function handler(req, res) {
   try {
-    const { query, offset, filters, filter } = req.query;
-    let url = `${process.env.BACKEND_URL_SEARCH}search?q=${query}`;
-    offset && (url += `&offset=${offset}`);
-    filters && filter && (url += `&${filters}=${filter}`);
+    const query = req?.url.split('?')[1];
+    const url = `${process.env.BACKEND_URL_SEARCH}search?${query}`;
     const response = await fetch(url);
     const data = await response.json();
     return res.status(200).json(data);
