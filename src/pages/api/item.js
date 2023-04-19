967a50c0ceef26fd333f93ffa6ed32fb592f6da7
MauricioMartinez <maoworshipper@gmail.com>
2023-04-19 12:46:37 -0500

    Add description api call to item handler

--- src/pages/api/item.js
+++ src/pages/api/item.js
@@ -1,12 +1,15 @@
 export default async function handler(req, res) {
-    try {
-      const { query } = req.query;
-      const url = `${process.env.BACKEND_URL_ITEM}${query}`;
-      const response = await fetch(url);
-      const data = await response.json();
-      return res.status(200).json(data);
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
+  try {
+    const { q } = req.query;
+    let url = `${process.env.BACKEND_URL_ITEM}${q}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    url = `${process.env.BACKEND_URL_ITEM}${q}/description`;
+    const responseDescription = await fetch(url);
+    const dataDescription = await responseDescription.json();
+    data.description = dataDescription.plain_text;
+    return res.status(200).json(data);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-  
+}
