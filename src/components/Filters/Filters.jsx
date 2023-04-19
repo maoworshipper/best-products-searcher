6758d53aa473b020b8d8fb8ec7d851d510c695e4
MauricioMartinez <maoworshipper@gmail.com>
2023-04-19 12:45:08 -0500

    Improvements and split logic and view  code Filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,62 +1,34 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addResults } from '@redux/productsSlice';
-import { BuildFilters } from './BuildFilters';
-import { useFetch } from '@hooks/useFetch';
+import { useFilters } from './hooks/useFilters';
+import { ActiveFilters } from './ActiveFilters';
 import styles from './Filters.module.scss';
 
 const Filters = () => {
-  const dispatch = useDispatch();
-  const { available_filters, query } = useSelector((state) => state.products) || {};
-  const [filters, setFilters] = useState([]);
-  const { data, stream, fetchData } = useFetch();
+  const { filtersOptions, handleFilter } = useFilters();
 
-  useEffect(() => {
-    const newFilters = available_filters.filter((filter) => {
-      return filter.id !== 'product';
-    });
-    setFilters(newFilters);
-  }, [available_filters]);
-
-  useEffect(() => {
-    console.log(filters);
-  }, [filters]);
-
-  const handleFilter = (filterId, valueId) => {
-    const url = `api/search?query=${query}&filters=${filterId}&filter=${valueId}`;
-    fetchData(url, {}, 'search');
+  const renderFilterOptions = (filter) => {
+    return filter.values.map((value) => (
+      <ul
+        className={styles.value}
+        key={value.id}
+        onClick={() => handleFilter(filter.id, value.id)}
+      >
+        <li className={styles.nameValue}>
+          {value.name}
+          <span className={styles.results}>{` (${value.results})`}</span>
+        </li>
+      </ul>
+    ));
   };
-
-  useEffect(() => {
-    if (data && stream === 'search') {
-      console.log(data);
-      dispatch(addResults(data));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data, stream]);
 
   return (
     <div className={styles.filters}>
-      {filters?.length > 0 &&
-        filters.map((filter) => (
+      <ActiveFilters />
+      {filtersOptions?.length > 0 &&
+        filtersOptions.map((filter) => (
           <div className={styles.filter} key={filter.id}>
             <h4 className={styles.name}>{filter.name}</h4>
             <div className={styles.valuesContainer}>
-              {filter.values.map((value) => (
-                <ul
-                  className={styles.value}
-                  key={value.id}
-                  onClick={() => handleFilter(filter.id, value.id)}
-                >
-                  <li className={styles.nameValue}>
-                    {value.name}
-                    <span
-                      className={styles.results}
-                    >{` (${value.results})`}</span>
-                  </li>
-                  {/* <span className={styles.results}>{value.results}</span> */}
-                </ul>
-              ))}
+              {renderFilterOptions(filter)}
             </div>
           </div>
         ))}
